dcf0e664a14dd49a218250b016c802f0589c4d73
wski <contact.mattdylan@gmail.com>
2015-12-06 18:56:41 -0600

    feat(Remote Config): Accepts remote config for easy updating

--- collectionService.js
+++ collectionService.js
@@ -5,8 +5,9 @@ const app = express();
 const trustedAddresses = require('./trustedConnections.js');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const request = require('request');
 
-const { authKey } = require('./hawkConfig');
+const { authKey, roosterAddr } = require('./hawkConfig');
 
 mongoose.connect('mongodb://localhost/hawk');
 
@@ -36,25 +37,33 @@ app.get('/prey/:id', function (req, res) {
   let address = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
   address = address
     .match(/\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b/g)[0];
-  
-  if(trustedAddresses.indexOf(address) > -1) {
-    let query = Nest.findOne({ 'id': req.params.id });
-    query.find(function (err, nest) {
-      if (err) return console.log(err);
-      if (nest.length > 0) {
-        res.jsonp(nest[0]);
-      }else {
-        res.status(404).jsonp({ 
-          error: "Nest not found. If you belive this is an error, please contact Matt." 
-        });
+   request(roosterAddr, function (error, response, body) {
+    if (error) {
+      console.log(error);
+    } else{
+      if(JSON.parse(body).error){
+        console.log(body);
+      }else{
+        if(JSON.parse(body)['trusted_connections'].indexOf(address) > -1) {
+          let query = Nest.findOne({ 'id': req.params.id });
+          query.find(function (err, nest) {
+            if (err) return console.log(err);
+            if (nest.length > 0) {
+              res.jsonp(nest[0]);
+            }else {
+              res.status(404).jsonp({ 
+                error: "Nest not found. If you belive this is an error, please contact Matt." 
+              });
+            }
+          });
+        }else {
+          res.status(500).jsonp({ 
+            error: "Prey rejected. You are not authorized to view this link. If you belive this is an error, please contact Matt." 
+          });
+        }
       }
-    });
-  }else {
-    res.status(500).jsonp({ 
-      error: "Prey rejected. You are not authorized to view this link. If you belive this is an error, please contact Matt." 
-    });
-  }
-  
+    }
+   });
 });
 
 app.post('/feed', function (req, res) {
@@ -66,47 +75,57 @@ app.post('/feed', function (req, res) {
   
   // If they pass the correct authKey and they are on the trusted address list..
   // ..then save the newly created data to the database for later use.
-  if(payload.authKey === authKey && trustedAddresses.indexOf(address) > -1) {
-    
-    // Find a nest by IP address, if none exist, create one in the DB.
-    let query = Nest.findOne({ 'address': address });
-    query.find(function (err, nest) {
-      if (err) return console.log(err);
-      if( nest.length > 0 ) {
-        // If a nest is found update and save it.
-        let activeNest = nest[0];
-        activeNest.cpu = payload.payload.cpu;
-        activeNest.hdd = payload.payload.hdd;
-        activeNest.mem = payload.payload.mem;
-        activeNest.save(function(err) {
-          if (err) console.log(err);
-        });
-      }else {
-        // if no nest is found, create one and save it.
-        nest = new Nest({
-          cpu: payload.payload.cpu,
-          mem: payload.payload.mem,
-          hdd: payload.payload.hdd,
-          name: payload.name,
-          address: address,
-          id: payload.id,
-          instanceType: payload.instanceType
-        });
-        nest.save(function(err){
-          if (err) console.log(err);
-          console.log(`new nest created for address: ${address}`);
-        })
+  request(roosterAddr, function (error, response, body) {
+    if (error) {
+      console.log(error);
+    } else{
+      if(JSON.parse(body).error){
+        console.log(body);
+      }else{
+        if(payload.authKey === authKey && JSON.parse(body)['trusted_connections'].indexOf(address) > -1) {
+          // Find a nest by IP address, if none exist, create one in the DB.
+          let query = Nest.findOne({ 'address': address });
+          query.find(function (err, nest) {
+            if (err) return console.log(err);
+            if( nest.length > 0 ) {
+              // If a nest is found update and save it.
+              let activeNest = nest[0];
+              activeNest.cpu = payload.payload.cpu;
+              activeNest.hdd = payload.payload.hdd;
+              activeNest.mem = payload.payload.mem;
+              activeNest.save(function(err) {
+                if (err) console.log(err);
+              });
+            }else {
+              // if no nest is found, create one and save it.
+              nest = new Nest({
+                cpu: payload.payload.cpu,
+                mem: payload.payload.mem,
+                hdd: payload.payload.hdd,
+                name: payload.name,
+                address: address,
+                id: payload.id,
+                instanceType: payload.instanceType
+              });
+              nest.save(function(err){
+                if (err) console.log(err);
+                console.log(`new nest created for address: ${address}`);
+              })
+            }
+          });
+          
+          res.jsonp({
+            success: true
+          });
+        }else {
+          res.status(500).jsonp({ 
+            error: "Payload rejected. This either means your connection address has changed or was never added to the trusted connections list." 
+          });
+        }
       }
-    });
-    
-    res.jsonp({
-      success: true
-    });
-  }else {
-    res.status(500).jsonp({ 
-      error: "Payload rejected. This either means your connection address has changed or was never added to the trusted connections list." 
-    });
-  }
+    }
+  });
+  
 });
 
 const server = app.listen(3000, function () {
--- hawkConfig.js
+++ hawkConfig.js
@@ -4,5 +4,6 @@ module.exports = {
   name: 'Dodo', // Can be any name used to identifiy this server by humans.
   refreshRate: 5000, // Time in milliseconds to refresh the data about the server.
   masterAddr: 'http://localhost:3000/feed',
+  roosterAddr: 'http://localhost:3001/',
   authKey: 'IWyT3W6c8TV8MY8cp4JjQlkFc60IUcMo'
 };
